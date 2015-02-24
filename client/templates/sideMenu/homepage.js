32793df386f9eacbaf808d8d6491b5550e4e6302
limingth <limingth@gmail.com>
2015-02-23 20:07:43 -0800

    Build template groups with lists

--- client/templates/sideMenu/homepage.js
+++ client/templates/sideMenu/homepage.js
@@ -1,3 +1,13 @@
+Template.groups.helpers({
+  times: function () {
+    var times = [];
+    _(10).times(function(n){
+      times.push(n);
+    });
+    return times;
+  }
+});
+
 Template.calendar.helpers({
   tmplName: function () {
     return Session.get('tmplName');  
@@ -20,4 +30,10 @@ Template.updates.helpers({
   tmplName: function () {
     return Session.get('tmplName');  
   }
+});
+
+Template.groups.events({
+  'click button': function (event, template) {
+    console.log ("Join groups clicked!");
+  },
 });
