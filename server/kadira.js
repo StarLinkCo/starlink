db637b4524f34f7c16b471fa6ae2a46b91dee747
James Wang <james@yangwang.us>
2015-03-03 00:32:02 -0800

    updated kadira packages

--- server/kadira.js
+++ server/kadira.js
@@ -1,2 +1,3 @@
-//Kadira.connect('mphd8DLknxspXWhH9', 'ece556b2-7ec9-4fd0-9886-3b1bd1ba1e5c')
+// meteor add meteorhacks:kadira
+Kadira.connect('mphd8DLknxspXWhH9', 'ece556b2-7ec9-4fd0-9886-3b1bd1ba1e5c')
 
