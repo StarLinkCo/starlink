337f8dddaac652e19882f631766cb81d4c7b7acc
James Wang <james@yangwang.us>
2015-03-14 01:40:01 -0700

    fixed timer

--- server/eventbritejob.js
+++ server/eventbritejob.js
@@ -7,14 +7,13 @@
 SyncedCron.add({
     name: 'Pull Eventbrite events to Event collection',
     schedule: function(parser) {
-        //return parser.text('every 2 minutes');
-        return parser.text('every 60 minutes');
+        return parser.text('every 30 minutes');
         //return parser.text('at 5:00 am every 1 day');
     },
 
     job: function() {
         _.each(Organizers.find().fetch(), function(organizer) {
-            console.log(organizer.name);
+            //console.log(organizer.name);
             var events = EJSON.parse(Meteor.http.call(
                 "GET",
                 "https://www.eventbrite.com/json/organizer_list_events?id=" +
