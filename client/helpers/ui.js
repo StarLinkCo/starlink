425a7046b7da21e8460f6d4fb5f55d029fb7c63a
Roc Yu <rociiu.yu@gmail.com>
2015-03-16 18:31:59 +0800

    rename ui helper to fix conflicts name

--- client/helpers/ui.js
+++ client/helpers/ui.js
@@ -2,6 +2,6 @@ UI.registerHelper('timeAgo', function(datetime) {
   Session.get('momentLocale'); // depend on session variable to reactively rerun the helper
   return moment(datetime).fromNow();
 });
-UI.registerHelper('userEmail', function(user) {
+UI.registerHelper('getUserEmail', function(user) {
   return getUserEmail(user);
 })
