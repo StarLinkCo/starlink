7751c1f16235d5a03bef13518f18ce5b08751cb6
James Wang <james@yangwang.us>
2015-03-16 01:15:27 -0700

    added mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yangwang on 3/16/15.
+ */
+App.info({
+    name: 'StarLink',
+    description: 'Online Startups Community',
+    version: '0.1.1'
+});
+
+App.icons({
+    'android_ldpi': 'public/logo-ldpi.png',
+    'android_mdpi': 'public/logo-mdpi.png',
+    'android_hdpi': 'public/logo-hdpi.png',
+    'android_xhdpi': 'public/logo-xhdpi.png'
+
+    //'iphone': 'public/logo-60.png',
+    //'iphone_2x': 'public/logo-60@2x.png'
+});
+/*
+App.launchScreens({
+    'iphone': 'public/logo~iphone.png',
+    'iphone_2x': 'public/logo@2x~iphone.png',
+    'iphone5': 'public/logo-568h@2x~iphone.png'
+});
+*/
