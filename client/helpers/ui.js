72488c393f9d6cc7d27e7b95423c1df5c08fea29
Roc Yu <rociiu.yu@gmail.com>
2015-03-16 18:30:07 +0800

    update profile to show info for user register with email

--- client/helpers/ui.js
+++ client/helpers/ui.js
@@ -2,3 +2,6 @@ UI.registerHelper('timeAgo', function(datetime) {
   Session.get('momentLocale'); // depend on session variable to reactively rerun the helper
   return moment(datetime).fromNow();
 });
+UI.registerHelper('userEmail', function(user) {
+  return getUserEmail(user);
+})
