e78a2fe889d313c7b41a0ae4c3627379e601fbd0
Roc Yu <rociiu.yu@gmail.com>
2015-03-18 21:01:09 +0800

    fix js error on login form due to wrong path, also give default data value to fix js error in ionic

--- both/router.js
+++ both/router.js
@@ -84,7 +84,11 @@ Router.map(function() {
   this.route('tabs.two', {path: '/tabs/two', layoutTemplate: 'tabsLayout'});
   this.route('tabs.three', {path: '/tabs/three', layoutTemplate: 'tabsLayout'});
   this.route('tabs.four', {path: '/tabs/four', layoutTemplate: 'tabsLayout'});
-  this.route('userAccounts');
+  this.route('userAccounts', {
+    data: function() {
+      return {};
+    }
+  });
 });
 
 var requireLoginHook = function () {
