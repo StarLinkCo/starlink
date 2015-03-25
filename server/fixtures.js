3b9db29030eb22350f61b0df53efbfda2653f933
James Wang <james@yangwang.us>
2015-03-25 01:26:36 -0700

    updated organizers

--- server/fixtures.js
+++ server/fixtures.js
@@ -55,3 +55,131 @@ if (Organizers.find({source: 'meetup'}).count() == 0) {
   });
 
 }
+
+/*
+{
+    "_id" : "5bGaNnGgsAm6xKyHP",
+    "name" : "SVIEF",
+    "flag" : "active",
+    "organizer_id" : "4302207107"
+}
+
+{
+    "_id" : "FG9uqqsyh3WNewQcW",
+    "name" : "TIPark Silicon Valley",
+    "flag" : "active",
+    "organizer_id" : "5628397911"
+}
+
+{
+    "_id" : "nRWWPbLK7oodYfgQQ",
+    "name" : "JayWSalon",
+    "flag" : "active",
+    "organizer_id" : "6428799777"
+}
+
+{
+    "_id" : "Joz2xKBx2WjyGzrPk",
+    "name" : "SVACE",
+    "flag" : "active",
+    "organizer_id" : "7462438955"
+}
+
+{
+    "_id" : "CKP4baLWJjH9SR35H",
+    "name" : "AngelsGlobal",
+    "flag" : "active",
+    "organizer_id" : "7684502595"
+}
+
+{
+    "_id" : "QPknSy7CvzrXNztFH",
+    "name" : "HYSTA",
+    "flag" : "active",
+    "organizer_id" : "7768017133"
+}
+
+{
+    "_id" : "yzzXE5BaEY4jCbZA8",
+    "name" : "SV Cafe",
+    "flag" : "active",
+    "organizer_id" : "7905018476"
+}
+
+{
+    "_id" : ObjectId("55052f9d6a4c3c3694035a18"),
+    "name" : "Yong",
+    "flag" : "active",
+    "organizer_id" : "6579662475"
+}
+
+{
+    "_id" : ObjectId("5505328c6a4c3c3694035a19"),
+    "name" : "Founders Space",
+    "flag" : "active",
+    "organizer_id" : "5321844957"
+}
+
+{
+    "_id" : ObjectId("550692d36a4c3c3694035a1a"),
+    "name" : "SVACE",
+    "flag" : "active",
+    "organizer_id" : "7862062877"
+}
+
+{
+    "_id" : ObjectId("5506fa156a4c3c3694035a1b"),
+    "name" : "Association of Chinese Entrepreneurs at Berkeley",
+    "flag" : "active",
+    "organizer_id" : "7919824397"
+}
+
+{
+    "_id" : ObjectId("55073860064405d02deecabc"),
+    "name" : "华人事业互助会 Bay Area Chapter",
+    "flag" : "active",
+    "organizer_id" : "7684661701"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541c8"),
+    "name" : "The Startup Conference",
+    "flag" : "active",
+    "organizer_id" : "480256426"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541c9"),
+    "name" : "Angel Launch",
+    "flag" : "active",
+    "organizer_id" : "582594815"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541ca"),
+    "name" : "Silicon Valley Chinese Technology & Business Association",
+    "flag" : "active",
+    "organizer_id" : "8012673825"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541cb"),
+    "name" : "Startup & Tech Mixer, Inc.",
+    "flag" : "active",
+    "organizer_id" : "6016680999"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541cc"),
+    "name" : "Startup Socials",
+    "flag" : "active",
+    "organizer_id" : "3922719157"
+}
+
+{
+    "_id" : ObjectId("550b7aa62f67244c68d541cd"),
+    "name" : "UP Global",
+    "flag" : "active",
+    "organizer_id" : "2300226659"
+}
+*/
