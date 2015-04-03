2467fbc50115b62deb62005949c4e5dd6d7837c6
Roc Yu <rociiu.yu@gmail.com>
2015-04-03 16:48:52 +0800

    track user login count, redirect to profile edit for first time, otherwise open calendar

--- client/main.js
+++ client/main.js
@@ -3,3 +3,13 @@ Meteor.subscribe("userData");
 Meteor.subscribe("tags");
 //Meteor.subscribe("groups");
 Meteor.subscribe("notifications", Meteor.userId());
+
+Accounts.onLogin(function(data) {
+  Meteor.call('increaseLogin', function(err, result) {
+    if (result == 1) {
+      Router.go('/profile?edit=1');
+    } else {
+      Router.go('/calendar');
+    }
+  });
+});
