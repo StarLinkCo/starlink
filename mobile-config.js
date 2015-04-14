c6072f076c3ecce0b64cddfa29ad86c3aa00ee45
James Wang <james@yangwang.us>
2015-04-14 00:11:15 -0700

    added ios icons

--- mobile-config.js
+++ mobile-config.js
@@ -13,10 +13,14 @@ App.icons({
     'android_ldpi': 'public/favicon-16x16.png',
     'android_mdpi': 'public/favicon-32x32.png',
     'android_hdpi': 'public/favicon-96x96.png',
-    'android_xhdpi': 'public/android-chrome-192x192.png'
+    'android_xhdpi': 'public/android-chrome-192x192.png',
+    'iphone': 'public/apple-touch-icon-57x57.png',
+    'iphone_2x': 'public/apple-touch-icon-114x114.png',
+    'iphone_3x': 'public/apple-touch-icon-180x180.png',
+    'ipad': 'public/apple-touch-icon-72x72.png',
+    'ipad_2x': 'public/apple-touch-icon-144x144.png'
 });
 
 App.accessRule("https://media.licdn.com", "true");
 App.accessRule("http://schd.ws", "true");
-
 
