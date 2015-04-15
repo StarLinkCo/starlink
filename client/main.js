0d2480459e1a3854ac305af6e320d7c1d079f608
James Wang <james@yangwang.us>
2015-04-15 01:31:15 -0700

    fixes

--- client/main.js
+++ client/main.js
@@ -8,6 +8,8 @@ Accounts.onLogin(function(data) {
   Meteor.call('increaseLogin', function(err, result) {
     if (result == 1) {
       Router.go('/profile?edit=1');
+    } else {
+      Router.go('/calendar');
     }
   });
 });
